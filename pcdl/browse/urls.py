788ed8cc2f440ac310e4ad2dbfcf17bdd117ba32
Kuda Kadira <kudakadira@Kudas-MacBook-Pro.local>
2020-10-20 20:06:25 +0100

    added user registration to site

--- pcdl/browse/urls.py
+++ pcdl/browse/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='browse-home'),
     path('latest/', views.latest, name='browse-latest'),
     path('videos/', views.videos, name='browse-videos'),
     path('audios/', views.audios, name='browse-audios'),
