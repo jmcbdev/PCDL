cafb26578e6e110723d516969c26542422acf949
Kuda Kadira <kudakadira@Kudas-MacBook-Pro.local>
2020-10-21 18:15:10 +0100

    updates to validations

--- pcdl/browse/views.py
+++ pcdl/browse/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Video
 
 
+def welcome(request):
+    return render(request, 'browse/welcome.html')
+
+@login_required    
 def index(request):
     context = {
         'videos': Video.objects.all()
     }
     return render(request, 'browse/home.html', context)
 
-
+@login_required
 def latest(request):
     context = {
         'videos': Video.objects.filter(latest=True)
     }
     return render(request, 'browse/latest.html', context)
-
+@login_required
 def videos(request):
     context = {
         'videos': Video.objects.filter(type_vid=True)
     }
     return render(request, 'browse/audios.html', context)
 
-
+@login_required
 def audios(request):
     context = {
         'videos': Video.objects.filter(type_vid=False)
--- pcdl/pcdl/urls.py
+++ pcdl/pcdl/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
+    path('', user_views.register, name='register'),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('browse.urls')),
